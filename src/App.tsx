9964c9f2128cc18442ebe42610a8ef057f15b2c4
santerilukka <156800715+santerilukka@users.noreply.github.com>
2026-01-12 18:53:11 +0200

    Refactor App component: temporarily replace main content with maintenance message and clean up commented code

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,43 @@ import { CurrentlyWorkingOn } from './components/layout/CurrentlyWorkingOn'
 import { Contact } from './components/layout/Contact'
 import { Footer } from './components/layout/Footer'
 import ClickSpark from './components/ui/effects/ClickSparkEffect'
+/*
+
+return (
+    <ClickSpark
+      sparkColor='#3d72e6'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={10}
+      duration={450}
+      extraScale={1.2}
+    >
+      <div className='min-h-screen bg-background text-foreground'>
+        {/* Navigation /*}
+        <Navigation />
+
+        {/* Main Content /*}
+        <main>
+          <Hero />
+          <AboutMe />
+          <TechStack />
+          <Education />
+          <Certificates />
+          <Projects />
+          <CurrentlyWorkingOn />
+          <Contact />
+        </main>
+
+        {/* Footer /*}
+        <Footer />
+
+        {/* Toast Notifications /*}
+        <Toaster />
+      </div>
+    </ClickSpark>
+  )
+
+*/
 
 export default function App() {
   useEffect(() => {
@@ -23,37 +60,5 @@ export default function App() {
     }
   }, [])
 
-  return (
-    <ClickSpark
-      sparkColor='#3d72e6'
-      sparkSize={10}
-      sparkRadius={15}
-      sparkCount={10}
-      duration={450}
-      extraScale={1.2}
-    >
-      <div className='min-h-screen bg-background text-foreground'>
-        {/* Navigation */}
-        <Navigation />
-
-        {/* Main Content */}
-        <main>
-          <Hero />
-          <AboutMe />
-          <TechStack />
-          <Education />
-          <Certificates />
-          <Projects />
-          <CurrentlyWorkingOn />
-          <Contact />
-        </main>
-
-        {/* Footer */}
-        <Footer />
-
-        {/* Toast Notifications */}
-        <Toaster />
-      </div>
-    </ClickSpark>
-  )
+  return <p>Under maintanance!</p>
 }
